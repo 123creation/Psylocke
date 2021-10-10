1cf29f5fcb3c3fab7861a48533b69dcfbb0a1362
123creation <90975856+123creation@users.noreply.github.com>
2021-10-10 15:57:34 +0530

    Update alive.py

--- Sophia/modules/alive.py
+++ Sophia/modules/alive.py
@@ -3,14 +3,14 @@ import re, os
 from Sophia.events import register
 from Sophia import telethn as tbot
 from Sophia import telethn as tgbot
-PHOTO = "https://telegra.ph/file/9ce5e51b9f4a531ea830b.jpg"
+PHOTO = "https://telegra.ph/file/24047dbb8187547305db6.jpg"
 @register(pattern=("/alive"))
 async def awake(event):
   PIKACHU = event.sender.first_name
   PIKACHU = "**♡ I,m Psylocke💖** \n\n"
   PIKACHU += "**♡ I'm Working Properly**\n\n"
-  PIKACHU += "**♡ Yuuki : 2.0 LATEST**\n\n"
-  PIKACHU += "**♡ My Master :** [Owner](t.me/Me_Iz_mad_boi)\n\n"
+  PIKACHU += "**♡ Psylocke : 2.0 LATEST**\n\n"
+  PIKACHU += "**♡ My Master :** [Besty](t.me/besty_Braddock)\n\n"
   PIKACHU += "**♡ Telethon Version : 1.23.0**\n\n"
   BUTTON = [[Button.url("𝙎𝙐𝙋𝙋𝙊𝙍𝙏", "https://t.me/PigasusSupport"), Button.url("𝙐𝙋𝘿𝘼𝙏𝙀", "https://t.me/PigasusUpdates")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=PIKACHU,  buttons=BUTTON)
